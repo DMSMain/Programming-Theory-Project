ff286a4c1b2850ded8e34a68471575d05ebfb141
DMSMain <105548836+DMSMain@users.noreply.github.com>
2022-06-02 00:33:15 +0200

    Update 1

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime); 
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float boundary = 4.4f;
+    private float flyForce = 0.5f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+        StayInBounds();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * flyForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void StayInBounds()
+    {
+        if (transform.position.y > boundary)
+        {
+            transform.position = new Vector2 (transform.position.x, boundary);
+            rb.velocity = Vector2.zero;
+        }
+
+        if (transform.position.y < -boundary - 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Destroy(gameObject);  
+    }
+}
